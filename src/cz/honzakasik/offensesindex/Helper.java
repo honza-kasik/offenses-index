d35a27d54cceb54afd4e148d4e7d0fa775e8433d
Honza Kašík <honza.kasik1@seznam.cz>
2015-11-04 19:45:32 +0100

    Added city selector dialog

---
+++ src/cz/honzakasik/offensesindex/Helper.java
@@ -0,0 +1,60 @@
+package cz.honzakasik.offensesindex;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static cz.honzakasik.offensesindex.DatabaseNames.*;
+import static cz.honzakasik.offensesindex.DatabaseNames.CITY;
+
+/**
+ * Created by Jan Kasik on 4.11.15.
+ */
+public abstract class Helper {
+
+    public static ObservableList<DriverTableItem> transformDriverTableData(ResultSet result) {
+        ObservableList<DriverTableItem> data = FXCollections.observableArrayList();
+        try {
+            while (result != null && result.next()) {
+                DriverTableItem driverTableItem = new DriverTableItem(
+                        result.getString(NAME),
+                        result.getString(SURNAME),
+                        result.getInt(POINT_COUNT),
+                        result.getInt(OFFENSES_COUNT),
+                        result.getString(CITY),
+                        result.getInt(ID));
+                data.add(driverTableItem);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public static ObservableList<String> transformCitySelectorData(ResultSet result) {
+        ObservableList<String> data = FXCollections.observableArrayList();
+        try {
+            while (result != null && result.next()) {
+                data.add(result.getString(CITY));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public static void displayNothingFoundError(Stage owner) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.initOwner(owner);
+        alert.initModality(Modality.WINDOW_MODAL);
+        alert.setTitle("Nebyly nalezeny žádné výsledky");
+        alert.setHeaderText("Pro zadané parametry nebyly nalezeny žádné výsledky!");
+        alert.setContentText("Opakujte prosím akci s jinými parametry.");
+        alert.showAndWait();
+    }
+}
